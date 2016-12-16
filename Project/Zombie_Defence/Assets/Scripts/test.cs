1674146aef4e3ac591f124d63d8e03e5d8023338
JinSuk Lee <4316510@naver.com>
2016-12-16 16:39:24 +0900

    적 이동 자연스럽게 해보자

---
+++ Project/Zombie_Defence/Assets/Scripts/test.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    [System.Serializable]
+    public class PathSettings
+    {
+        public Transform[] top_paths;
+        public Transform[] bottom_paths;
+        public Transform[] right_paths;
+    }
+
+    [SerializeField]
+    public PathSettings paths;
+
+    void OnEnable()
+    {
+        // 부모 객체 생성하고 패치 설정
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(MovePath());
+        }
+    }
+
+    IEnumerator MovePath()
+    {
+        int path_index = 0;
+
+        while (path_index < paths.top_paths.Length)
+        {
+            // 이동
+            transform.LookAt(paths.top_paths[path_index]);
+            transform.Translate(0f, 0f, Time.deltaTime * 10f);
+
+            // 거리 같아지면 다음으로 이동
+            if (Vector3.Distance(transform.position, paths.top_paths[path_index].position) < 1f)
+            {
+                path_index++;
+            }
+
+            yield return null;
+        }
+    }
+}
--- Readme.txt
+++ Readme.txt
@@ -1,1 +1,4 @@
-﻿
+﻿Simple Military - Cartoon War
+Simple Zombies - Cartoon Characters v1.03
+SimpliCity Weapons Explosives
+SimpliCity Weapons Explosives II
